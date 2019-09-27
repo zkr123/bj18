c9ed9b772c0b247274b481f4165bf222cc16dfe2
z <1363685155@qq.com>
2019-09-27 09:55:29 +0800

    创建视图login

--- views.py
+++ views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
-
+from django.shortcuts import redirect
 
 def index(resquest):
     return HttpResponse('index')
+
+def login(request):
+    return redirect('/index')
